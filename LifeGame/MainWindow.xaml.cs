3c32781b37f0a67aa088286377107b875ab5e440
psy0370 <52093529+psy0370@users.noreply.github.com>
2020-07-11 22:06:27 +0900

    プロジェクト ファイルを追加します。

---
+++ LifeGame/MainWindow.xaml.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace LifeGame
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const int CellPixels = 6;
+
+        private Task loopThread;
+        private CancellationTokenSource tokenSource;
+        private Models.Cells cells;
+        volatile int generationFPS;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// ウインドウを読み込んだときの処理を定義します。
+        /// </summary>
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (!int.TryParse(HorizonCells.Text, out int h) || !int.TryParse(VerticalCells.Text, out int v))
+            {
+                return;
+            }
+
+            cells = new Models.Cells(h, v);
+            generationFPS = 10;
+            BuildCellView(h, v);
+            MainLoopThread();
+        }
+
+        /// <summary>
+        /// ボタンをクリックしたときの処理を定義します。
+        /// </summary>
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (loopThread?.IsCompleted == true)
+            {
+                MainLoopThread();
+            }
+            else
+            {
+                tokenSource.Cancel();
+            }
+        }
+
+        /// <summary>
+        /// セルの表示領域でマウスホイールを操作したときの処理を定義します。
+        /// </summary>
+        private void CellView_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            if (e.Delta > 0 && generationFPS < 60)
+            {
+                generationFPS++;
+            }
+            else if (e.Delta < 0 && generationFPS > 5)
+            {
+                generationFPS--;
+            }
+        }
+
+        /// <summary>
+        /// セルの表示領域を生成します。
+        /// </summary>
+        /// <param name="h">セルの横の個数を設定します。</param>
+        /// <param name="v">セルの縦の個数を設定します。</param>
+        private void BuildCellView(int h, int v)
+        {
+            // 縦横いずれかが10未満の場合は例外をスロー
+            if (h < 10 || v < 10)
+            {
+                throw new ArgumentException();
+            }
+
+            CellView.Width = h * CellPixels - 1;
+            CellView.Height = v * CellPixels - 1;
+            CellView.Children.Clear();
+
+            for (var y = 0; y < v; y++)
+            {
+                for (var x = 0; x < h; x++)
+                {
+                    var rectangle = new Rectangle()
+                    {
+                        Width = CellPixels - 1,
+                        Height = CellPixels - 1
+                    };
+                    rectangle.MouseDown += Rectangle_MouseDown;
+                    Canvas.SetLeft(rectangle, x * CellPixels);
+                    Canvas.SetTop(rectangle, y * CellPixels);
+                    CellView.Children.Add(rectangle);
+                }
+            }
+
+            return;
+        }
+
+        /// <summary>
+        /// セルをクリックしたときの処理を定義します。
+        /// </summary>
+        private void Rectangle_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (loopThread?.IsCompleted == true)
+            {
+                var cell = e.Source as Rectangle;
+                int index = CellView.Children.IndexOf(cell);
+                cells.Conditions[index] = !cells.Conditions[index];
+                cell.Fill = cells.Conditions[index] ? Brushes.Black : Brushes.White;
+            }
+        }
+
+        /// <summary>
+        /// メインループを定義します。
+        /// </summary>
+        private void MainLoopThread()
+        {
+            tokenSource = new CancellationTokenSource();
+            loopThread = Task.Factory.StartNew(token =>
+            {
+                // キャンセルトークンが無効の間ループ
+                while (!((CancellationToken)token).IsCancellationRequested)
+                {
+                    Dispatcher.BeginInvoke(new Action(() =>
+                    {
+                        // 生存セルを点灯
+                        for (var i = 0; i < cells.Conditions.Length; i++)
+                        {
+                            var rectangle = (Rectangle)CellView.Children[i];
+                            rectangle.Fill = cells.Conditions[i] ? Brushes.Black : Brushes.White;
+                        }
+                        cells.CalcNextGeneration();
+                    }));
+
+                    // FPS設定に応じてスリープ
+                    Thread.Sleep(1000 / generationFPS);
+                }
+            }, tokenSource.Token).ContinueWith(t =>
+            {
+                EditButton.Dispatcher.Invoke(() =>
+                {
+                    EditButton.Content = "開始";
+                });
+            });
+
+            EditButton.Content = "編集";
+        }
+    }
+}
---
+++ LifeGame/Models/Cells.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LifeGame.Models
+{
+    /// <summary>
+    /// セルの状態を表すクラスを定義します。
+    /// </summary>
+    public class Cells
+    {
+        /// <summary>
+        /// セルの状態を取得します。
+        /// </summary>
+        public bool[] Conditions { get; private set; }
+
+        /// <summary>
+        /// 一次元配列上における8方向のインデックスの差分を格納するコレクションを定義します。
+        /// </summary>
+        private readonly int[] directions = new int[8];
+
+        /// <summary>
+        /// <see cref="Cells"/>クラスの新しいインスタンスを初期化します。
+        /// </summary>
+        /// <param name="h">セルの横の個数を設定します。</param>
+        /// <param name="v">セルの縦の個数を設定します。</param>
+        public Cells(int h, int v)
+        {
+            // セルの常置を初期化
+            Conditions = Enumerable.Repeat(false, h * v).ToArray();
+
+            // 各方向の移動量を設定
+            directions[0] = -h - 1;
+            directions[1] = -h;
+            directions[2] = -h + 1;
+            directions[3] = -1;
+            directions[4] = 1;
+            directions[5] = h - 1;
+            directions[6] = h;
+            directions[7] = h + 1;
+
+            // セルに初期値を設定
+            Conditions[0 * h + 1] = true;
+            Conditions[1 * h + 2] = true;
+            Conditions[2 * h + 0] = true;
+            Conditions[2 * h + 1] = true;
+            Conditions[2 * h + 2] = true;
+        }
+
+        /// <summary>
+        /// <see cref="Conditions"/>プロパティで表されるセルの世代を一つ進めます。
+        /// </summary>
+        public void CalcNextGeneration()
+        {
+            var results = new bool[Conditions.Length];
+
+            foreach (var (count, index) in GetExistCellCounts().Select((value, index) => (value, index)))
+            {
+                if (!Conditions[index])
+                {
+                    results[index] = count == 3;
+                }
+                else if (Conditions[index])
+                {
+                    results[index] = count == 2 || count == 3;
+                }
+            }
+
+            for (var i = 0; i < results.Length; i++)
+            {
+                Conditions[i] = results[i];
+            }
+        }
+
+        /// <summary>
+        /// セルの周囲の生存セルをカウントします。
+        /// </summary>
+        /// <returns>各セルの周囲の生存セルの個数を格納する列挙可能なコレクションを返します。</returns>
+        private IEnumerable<int> GetExistCellCounts()
+        {
+            for (var i = 0; i < Conditions.Length; i++)
+            {
+                var count = 0;
+
+                foreach (var dir in directions)
+                {
+                    if (i + dir < 0)
+                    {
+                        if (Conditions[i + dir + Conditions.Length])
+                        {
+                            count++;
+                        }
+                    }
+                    else if (i + dir >= Conditions.Length)
+                    {
+                        if (Conditions[i + dir - Conditions.Length])
+                        {
+                            count++;
+                        }
+                    }
+                    else if (Conditions[i + dir])
+                    {
+                        count++;
+                    }
+                }
+
+                yield return count;
+            }
+        }
+    }
+}
